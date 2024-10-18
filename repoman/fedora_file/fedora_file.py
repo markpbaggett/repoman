bda98992ad6ed0172b156d75b61af9d7dfc59bce
Mark Baggett <markpbaggett@gmail.com>
2024-10-18 08:52:27 -0500

    Get Base64 Cantaloupe request.

--- repoman/fedora_file/fedora_file.py
+++ repoman/fedora_file/fedora_file.py
@@ -2,10 +2,11 @@ import httpx
 from rdflib import URIRef
 from repoman.fedora_object import FedoraObject
 import os
+import base64
 
 
 class FedoraFile(FedoraObject):
-    def __init__(self, uri, position):
+    def __init__(self, uri, position=0):
         super().__init__(uri)
         self.position = position
 
@@ -18,6 +19,12 @@ class FedoraFile(FedoraObject):
         return list(self.content.objects(
             subject=URIRef(self.uri), predicate=URIRef(f"{self.namespaces.ldp}contains")
         ))
+
+    def get_cantaloupe_base_64(self):
+        server = self.uri.split('/')[2]
+        encoded = base64.urlsafe_b64encode(bytes(self.uri, "utf-8"))
+        decoded = encoded.decode("utf-8")
+        return f"https://{server}/iiif/2/{decoded}/info.json"
 
     def download(self, path):
         file_path = str(list(self.content.objects(
