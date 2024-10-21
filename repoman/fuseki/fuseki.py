f0ec4c9660b89d4bb6d5121d6c11384445d44b1f
Mark Baggett <markpbaggett@gmail.com>
2024-10-21 08:13:58 -0500

    Add get distinct last modified.

--- repoman/fuseki/fuseki.py
+++ repoman/fuseki/fuseki.py
@@ -1,6 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 from rdflib import URIRef, Graph, Literal
 
+
 class FusekiQuery:
     def __init__(self, sparql_endpoint):
         self.sparql_endpoint = SPARQLWrapper(sparql_endpoint)
@@ -28,6 +29,20 @@ class FusekiQuery:
             )
         return results["results"]["bindings"]
 
+    def get_distinct_last_modified(self):
+        query = f"""
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX ebucore: <http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#>
+        SELECT DISTINCT ?o  WHERE {{
+            ?s <http://fedora.info/definitions/v4/repository#lastModified> ?o .
+        }} 
+        """
+        self.sparql_endpoint.setQuery(query)
+        self.sparql_endpoint.setReturnFormat(JSON)
+        results = self.sparql_endpoint.query().convert()
+        return [value['o']['value'] for value in results["results"]["bindings"]]
+
     def __build_graph(self, subject, results):
         final = Graph()
         for result in results:
@@ -41,10 +56,11 @@ class FusekiQuery:
 
 
 if __name__ == "__main__":
-    x = FusekiQuery(sparql_endpoint="https://api.library.tamu.edu/fcrepo-fuseki/fcrepo/query")
-    print(
-        x.get_predicates_and_objects(
-            "https://api.library.tamu.edu/fcrepo/rest/3b/6f/c3/25/3b6fc325-f6ca-41d8-b91e-8c5db3be8c13/BasbanesEternalPassion_objects/13",
-            return_graph=True
-        )
-    )
+    x = FusekiQuery(sparql_endpoint="https://api-dev.library.tamu.edu/fcrepo-fuseki/fcrepo/query")
+    # print(
+    #     x.get_predicates_and_objects(
+    #         "https://api-dev.library.tamu.edu/fcrepo/rest/3b/6f/c3/25/3b6fc325-f6ca-41d8-b91e-8c5db3be8c13/fb/00/6c/bc/fb006cbc-d2e5-4fcb-9321-62b6385720d0/berger_cloonan_batch_5_objects/241/orderProxies/page_0_proxy",
+    #         return_graph=True
+    #     )
+    # )
+    print(x.get_distinct_last_modified())
